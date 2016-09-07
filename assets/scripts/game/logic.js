0395f4dfef1de13b780a287d424c7987c4c6ca52
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-07 18:23:41 -0400

    Write first win condition
    Prove a working approach to testing for win conditions.
    Entering three matching values in the first row now results in the next
    server request returning a game object with 'over: true'

--- assets/scripts/game/logic.js
+++ assets/scripts/game/logic.js
@@ -27,6 +27,25 @@ const isLegalMove = function (index) {
   return true;
 };
 
+const isOver = function (index, value) {
+  // create a copy of the cells array so we can check for a win
+  // without altering the actual array - only a server response should do that
+  let cells = app.user.game.cells.slice();
+
+  // apply the most recent play to our copy
+  cells[index] = value;
+  console.log("fake cells is ", cells);
+  // check if that play results in a win
+  if (cells[0] === cells[1] && cells[0] === cells[2]) {
+    console.log("i'm in the true block!");
+    return true;
+  } else {
+    console.log("i'm in the false block!");
+    return false;
+  }
+
+};
+
 // Processes a single game turn from either player
 const processTurn = function (index) {
   console.log("you are in processTurn");
@@ -38,18 +57,22 @@ const processTurn = function (index) {
   }
 
   // if we get here, it's a legal move
-  // now we need to figure out some parameters to pass to updateGame
 
-  // should we tell updateGame to put an 'x' or an 'o' in the cell?
-  // this could potentially be broken out in a
+  // decide whether to put an 'x' or an 'o' in the cell
+  // this could potentially be broken out in a separate function
   let value = 'x';
   if (isPlayerXTurn === false) {
     value = 'o';
   }
   isPlayerXTurn = !isPlayerXTurn;
 
+  // now decide whether it's a win or not
+
+  let over = isOver(index, value);
+  console.log("over is ", over);
+
   // now we can call updateGame
-  updateGame(index, value, false);
+  updateGame(index, value, over);
 };
 
 module.exports = {
