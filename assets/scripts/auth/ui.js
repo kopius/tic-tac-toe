b27cb7c6adf5a06821fc38ee165155539376d6f4
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-08 12:05:10 -0400

    Hide/Show authentication forms on login/logout
    -sign up and sign in forms are now hidden when user logs in,
    and shown when user logs out (and shown by default at page load)

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,31 +5,49 @@ const app = require('../app');
 const signUpSuccess = (data) => {
   //display success message in the UI and prompt them to sign in
 
+  // console checks
   console.log("Signed up successfully");
   console.log("data is ", data);
 };
 
 const signInSuccess = (data) => {
+  // store new user in app.user
   app.user = data.user;
+
+  // hide the login forms and reveal the game board
+  $('#auth-forms-box').hide();
   $('#game-board').show();
 
+  // console checks
   console.log("Signed in successfully");
   console.log("app.user is ", app.user);
 };
 
 const signOutSuccess = () => {
+  // clear the user object
   app.user = null;
-  $('#game-board').hide();
 
+  // hide the game board and display the login forms
+  $('#game-board').hide();
+  $('#auth-forms-box').show();
+
+  // display success message in the UI
+
+  // console checks
   console.log("Signed out successfully");
   console.log("app.user is ", app.user);
 };
 
 const changePasswordSuccess = () => {
+  // display success message in the UI
+
+  // console checks
   console.log("Password changed successfully");
 };
 
 const failure = (error) => {
+
+  // console checks
   console.log("FAILURE");
   console.error(error);
 };
