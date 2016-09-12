12b519e07a13557c580b4ba7463b56072512a759
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-12 01:29:19 -0400

    Switch to game status view when game ends
    -switch to game status view when a game ends, to ensure that
    the user knows when the game has ended
    -add comments to game UI functions and clean up code

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -2,40 +2,40 @@
 
 const app = require('../app');
 
-
-const paintCell = function (element, index) {
+// Applies a value to a single cell of the game board
+const paintCell = function (value, index) {
   let id = index + 1;
   let cellId = '#cell' + id;
-  $(cellId).text(element);
+  $(cellId).text(value);
 };
 
-
+// Applies the current game state to the cells of the game board
 const paintBoard = function (cells) {
   cells.forEach(paintCell);
 };
 
-
+// Clears the cells of the game board
 const resetBoard = function () {
   let emptyCells = ['', '', '', '', '', '', '', '', ''];
   paintBoard(emptyCells);
 };
 
-
+// Displays an alert message to the user
 const displayAlert = function (alert) {
   $('#misc-message').text(alert);
 };
 
-
+// Clears any alert text from the UI
 const resetAlert = function () {
   $('#misc-message').text('');
 };
 
-
+// Displays the current active player
 const displayWhoseTurn = function () {
   $('#active-player').text("player " + app.activePlayer + '\'s');
 };
 
-
+// Changes the active player
 const changeWhoseTurn = function () {
   if (app.activePlayer === 'x') {
     app.activePlayer = 'o';
@@ -44,14 +44,14 @@ const changeWhoseTurn = function () {
   }
 };
 
-
+// Displays the winner of the game
 const displayWinMessage = function () {
   $('#whose-turn').hide();
   $('#win-message').show();
   $('#winning-player').text(app.winner);
 };
 
-
+// Initializes the game view
 const initializeGameView = function() {
   resetAlert();
   displayWhoseTurn();
@@ -61,21 +61,21 @@ const initializeGameView = function() {
   $('#info-bar-buttons').show();
 };
 
-
+// Displays the status of the current game
 const showGameStatusView = function () {
   resetAlert();
   $('#game-status-view').show();
   $('#profile-view').hide();
 };
 
-
+// Displays the profile of the current user
 const showProfileView = function () {
   resetAlert();
   $('#game-status-view').hide();
   $('#profile-view').show();
 };
 
-
+// Resets the game state and the UI for the beginning of a new game
 const createGameSuccess = function (data) {
   app.user.game = data.game;
 
@@ -91,13 +91,12 @@ const createGameSuccess = function (data) {
   showGameStatusView();
 };
 
-
+// Displays an alert to the user when createGame fails
 const createGameFailure = function () {
-  $('#misc-message').text("failed to create game");
+  displayAlert("failed to create game");
 };
 
-
-// This function fires every turn, updating the UI to reflect the latest move
+// Updates the game state and UI to reflect the latest move
 const updateGameSuccess = function (data) {
   // update the game object in app.js and assign to a local variable
   app.user.game = data.game;
@@ -110,6 +109,7 @@ const updateGameSuccess = function (data) {
   // if the game is over, display a win message
   if (game.over) {
     displayWinMessage();
+    showGameStatusView();
     return;
   }
 
@@ -120,12 +120,12 @@ const updateGameSuccess = function (data) {
   displayWhoseTurn();
 };
 
-
+// Displays an alert to the user when updateGame fails
 const updateGameFailure = function () {
-  $('#misc-message').text('failed to update the game');
+  displayAlert('failed to update the game');
 };
 
-
+// Updates and displays the profile of the current user
 const showProfile = function(data) {
   app.user.games = data.games;
   let games = app.user.games;
@@ -138,7 +138,7 @@ const showProfile = function(data) {
   showProfileView();
 };
 
-
+// Displays an alert to the user when indexGames fails
 const indexGamesFailure = function() {
   displayAlert('cannot display profile at this time');
 };
